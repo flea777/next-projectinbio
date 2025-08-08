7de182e9f290155a1c1ae442da4e4fcb943339b2
Pedro Bim <pedrodinizz2@outlook.com>
2025-08-08 13:19:00 -0300

    Config: fonte RedHatDisplay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
 import './globals.css'
 
+import { Red_Hat_Display } from 'next/font/google'
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="pt-BR">
+      <body className={`${redHatDisplay.className} antialiased`}>
+        {children}
+      </body>
     </html>
   )
 }
