5fda00c026e5ecf755051f10f1a941c0ff6a7dfe
Pedro Bim <pedrodinizz2@outlook.com>
2025-08-10 12:05:52 -0300

    Componentes: Pricing e VideoExplanation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from './components/landing-page/Header'
 import { Hero } from './components/landing-page/Hero'
+import { VideoExplanation } from './components/landing-page/VideoExplanation'
+import { Pricing } from './components/landing-page/Pricing'
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
+      <VideoExplanation />
+      <Pricing />
     </div>
   )
 }
