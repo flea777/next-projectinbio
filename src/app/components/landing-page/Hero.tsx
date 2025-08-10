2a9c0727f7c068b04a143a1111ca1a283151685a
Pedro Bim <pedrodinizz2@outlook.com>
2025-08-10 01:26:29 -0300

    Componente: UserCard / Continuação da Hero Section

--- src/app/components/landing-page/Hero.tsx
+++ src/app/components/landing-page/Hero.tsx
@@ -1,7 +1,11 @@
+import { UserCard } from '../commons/UserCard'
+import { Button } from '../ui/Button'
+import { TextInput } from '../ui/TextInput'
+
 export function Hero() {
   return (
-    <div className="flex border">
-      <div className="w-full flex flex-col gap-2 mt-[35vh] border">
+    <div className="flex h-screen">
+      <div className="w-full flex flex-col gap-2 mt-[35vh]">
         <h1 className="text-5xl font-bold text-white leading-[64px]">
           Seus projetos e redes sociais em um único link
         </h1>
@@ -12,18 +16,18 @@ export function Hero() {
           Acompanhe o engajamento com Analytics de cliques
         </h2>
 
-        <div className="flex items-center gap-2 w-full -mt-[10vh]">
+        <div className="flex items-center gap-2 w-full mt-[10vh]">
           <span className="text-white text-xl">projectinbio.com</span>
 
-          <input type="text" />
+          <TextInput placeholder="Seu link" />
 
-          <button>Criar agora</button>
+          <Button>Criar agora</Button>
         </div>
       </div>
 
-      <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB, transparent_55%)]">
+      <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
         <div className="relative">
-          {/* <UserCard /> */}
+          <UserCard />
 
           <div className="absolute -bottom-[7%] -right-[45%]">
             {/* <TotalVisits /> */}
