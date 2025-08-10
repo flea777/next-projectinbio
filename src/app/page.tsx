8b3f987c815ddb37dd492bb22542030bf520a590
Pedro Bim <pedrodinizz2@outlook.com>
2025-08-10 11:42:34 -0300

    Componente: Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Header } from './components/landing-page/Header'
 import { Hero } from './components/landing-page/Hero'
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
-    <Hero />
+      <Header />
+      <Hero />
     </div>
   )
 }
