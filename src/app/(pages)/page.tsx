005d23fea3fcf368c694595e43db1cbf26d49c48
Pedro Bim <pedrodinizz2@outlook.com>
2025-08-10 13:39:26 -0300

    Página: criar link

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,17 @@
+import { Header } from '../components/landing-page/Header'
+import { Hero } from '../components/landing-page/Hero'
+import { VideoExplanation } from '../components/landing-page/VideoExplanation'
+import { Pricing } from '../components/landing-page/Pricing'
+import { Faq } from '../components/landing-page/Faq'
+
+export default function Home() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Header />
+      <Hero />
+      <VideoExplanation />
+      <Pricing />
+      <Faq />
+    </div>
+  )
+}
