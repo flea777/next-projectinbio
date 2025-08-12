ab322b8691e575c69b46e736a1948b64163e5831
Pedro Bim <pedrodinizz2@outlook.com>
2025-08-12 13:00:04 -0300

    Estilização do modal

--- app/(pages)/[profileId]/NewProject.tsx
+++ app/(pages)/[profileId]/NewProject.tsx
@@ -1,8 +1,11 @@
 'use client'
 
-import { Plus } from 'lucide-react'
+import { ArrowUpFromLine, Plus } from 'lucide-react'
 import { useState } from 'react'
 import { Modal } from '../../components/ui/Modal'
+import { TextInput } from '../../components/ui/TextInput'
+import { Button } from '../../components/ui/Button'
+import { TextArea } from '../../components/ui/TextArea'
 
 export function NewProject({ profileId }: { profileId: string }) {
   const [isOpen, setIsOpen] = useState(false)
@@ -21,7 +24,64 @@ export function NewProject({ profileId }: { profileId: string }) {
         <span>Novo projeto</span>
       </button>
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <div className="border p-10">Hello World</div>
+        <div className="bg-[#050505] p-8 rounded-[20px] flex flex-col justify-between gap-10">
+          <p className="text-white font-bold text-xl">Novo projeto</p>
+          <div className="flex gap-10">
+            <div className="flex flex-col items-center gap-3 text-xs">
+              <div className="w-[100px] h-[100px] rounded-xl bg-background-tertiary overflow-hidden">
+                <button className="w-full h-full">100x100</button>
+              </div>
+              <button className="text-white flex items-center gap-2">
+                <ArrowUpFromLine className="size-4" />
+                <span>Adicionar imagem</span>
+              </button>
+              <input
+                type="file"
+                id="imageInput"
+                accept="image/*"
+                className="hidden"
+              />
+            </div>
+            <div className="flex flex-col gap-4 w-[293px]">
+              <div className="flex flex-col gap-1">
+                <label htmlFor="project-name" className="text-white font-bold">
+                  Titulo do projeto
+                </label>
+                <TextInput
+                  id="project-name"
+                  placeholder="Digite o nome do projeto"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor="project-description"
+                  className="text-white font-bold"
+                >
+                  Descrição
+                </label>
+                <TextArea
+                  id="project-description"
+                  placeholder="Dê uma breve descrição do seu projeto"
+                  className="h-36"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <label htmlFor="project-url" className="text-white font-bold">
+                  URL do projeto
+                </label>
+                <TextInput
+                  type="url"
+                  id="project-description"
+                  placeholder="Digite a URL do projeto"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4 justify-end">
+            <button className="font-bold text-white">Voltar</button>
+            <Button>Salvar</Button>
+          </div>
+        </div>
       </Modal>
     </>
   )
